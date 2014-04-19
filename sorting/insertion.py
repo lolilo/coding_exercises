105b7aeb80c9b8a84dd4767f35171a604b54b0ee
Hackbright Student <info@hackbrightacademy.com>
2014-04-18 19:54:17 -0700

    comments

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -1,18 +1,4 @@
-def insertsion_sort(l):
-    for i in range(len(l)-1):
-        p1 = l[i+1]
-        count = 0
-        while p1 < l[i-count] and ((i-count)>=0):
-            l[i-count+1] = l[i-count]
-            # l[i-count+1]=p1
-            count += 1
-        l[i-count+1]=p1
-
-l = [9,8,9,6,1]
-insertsion_sort(l)
-
-print l
-
+# TODO: recode
 def insertionSort(alist):
    for index in range(1,len(alist)):
 
@@ -28,3 +14,24 @@ def insertionSort(alist):
 alist = [54,26,93,17,77,31,44,55,20]
 insertionSort(alist)
 print(alist)
+
+
+
+
+# # blarrghhh way
+
+# def insertsion_sort(l):
+#     for i in range(len(l)-1):
+#         p1 = l[i+1]
+#         count = 0
+#         while p1 < l[i-count] and ((i-count)>=0):
+#             l[i-count+1] = l[i-count]
+#             # l[i-count+1]=p1
+#             count += 1
+#         l[i-count+1]=p1
+
+# l = [9,8,9,6,1]
+# insertsion_sort(l)
+
+# print l
+
