0eea3357be41b406ad01bd1a3db37bfd04df3cb5
Hackbright Student <info@hackbrightacademy.com>
2014-04-21 12:24:46 -0700

    comments

--- search/binary_search.py
+++ search/binary_search.py
@@ -3,7 +3,7 @@ def binary_search(l, t):
     while not found and len(l) > 0:
         i = len(l)//2
         if t > i: 
-            l = l[i+1:]
+            l = l[i+1:] #why doesn't this give an index error
         elif t < i:
             l = l[:i]
         else:
@@ -11,6 +11,18 @@ def binary_search(l, t):
     return found
 
 
+
+def binary_search2(l, t):
+    while len(l) > 0:
+        i = len(l)//2
+        if t > i: 
+            l = l[i+1:] #why doesn't this give an index error
+        elif t < i:
+            l = l[:i]
+        else:
+            return True
+    return False
+
 l = range(5)
 
-print binary_search(l, 2)
+print binary_search(l, 20)
