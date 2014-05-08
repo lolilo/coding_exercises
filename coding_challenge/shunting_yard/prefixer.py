29d47c91fcb57d16ab8f630d829cb4464fec427f
Hackbright Student <info@hackbrightacademy.com>
2014-05-07 18:24:02 -0700

    recursionnnnn

--- coding_challenge/shunting_yard/prefixer.py
+++ coding_challenge/shunting_yard/prefixer.py
@@ -1,6 +1,20 @@
 import re
 num_pattern = re.compile('[0-9]+')
-op_pattern = re.compile('[*+-/%]')
+op_pattern = re.compile('[*+-/]')
+paren_pattern = re.compile('[()]')
+
+# PRECEDENCE = {
+#     '+': 1,
+#     '-': 1,
+#     '/': 2,
+#     '*': 2
+# }
+
+def precedence(operator):
+    if operator in ['+', '-']:
+        return 1
+    if operator in ['*', '/']:
+        return 2
 
 def parse(user_input):
     tokens = user_input.split()
@@ -11,38 +25,66 @@ def create_out_queue(user_input):
 
     out_queue = []
     op_stack = []
-    # postfix = ''
 
     while tokens:
         token = tokens.pop(0)
-        if num_pattern.match(token): #this breaks...passes for 6+5 -- still matches 6. need to ensure tokens do not mix op and num
+        if num_pattern.match(token):
             out_queue.append(token)
         elif op_pattern.match(token):
-            op_stack.append(token)
+            if not op_stack or precedence(op_stack[-1]) < precedence(token):
+                op_stack.append(token)
+            elif precedence(op_stack[-1]) >= precedence(token):
+                to_queue = op_stack.pop()
+                out_queue.append(to_queue)
+                op_stack.append(token)
+            else: 
+                print "WHAT IS THIS.", token
         else:
             print "Unexpected token %r" %token
     # output to reverse polish notation/RPN
     while op_stack:
         operator = op_stack.pop()
         out_queue.append(operator)
+    print out_queue
     return out_queue
 
-def infix_to_postfix(user_input):
-    out_queue = create_out_queue(user_input)
-    return out_string(out_queue)
-
 # recursively traverse out_queue
-def out_string(queue):
-    if len(queue) == 3:
+# def out_string(queue):
+    # if len(queue) == 3: # base case
+    #     operator = queue[2]
+    #     arg1 = queue[0]
+    #     arg2 = queue[1]
+    #     return '(' + arg1 + ' ' + arg2 + ' ' + operator + ')'
+
+    # elif num_pattern.match(queue[-1]): # number
+    #     return queue[-1]
+
+    # operator = queue[-1]
+    # exp = out_string(queue[:-1])
+    # print 'operator', operator
+    # print exp
+    # return '(' + exp + operator + ')'
+
+def queue_to_postfix(queue):
+
+    if not queue:
+        return ''
+
+    if len(queue) == 3: # base case
         operator = queue[2]
         arg1 = queue[0]
         arg2 = queue[1]
         return '(' + arg1 + ' ' + arg2 + ' ' + operator + ')'
-    operator = queue[-1]
-    arg1 = queue[0]
-    arg2 = out_string(queue[1:-1])
-    return '(' + arg1 + ' ' + arg2 + ' ' + operator + ')'
 
-s = '3 + 4 * 2'
+    out = '('
+    first = queue.pop(0)
+    while not op_pattern.match(first):
+        out += first + ' '
+        first = queue.pop(0)
+    # operator found
+    out += first + ')'
+    return '(' + out + queue_to_postfix(queue) + ')'
 
-print infix_to_postfix(s)
+def infix_to_postfix(user_input):
+    out_queue = create_out_queue(user_input)
+    return queue_to_postfix(out_queue)
--- coding_challenge/shunting_yard/unit_tests.py
+++ coding_challenge/shunting_yard/unit_tests.py
@@ -6,12 +6,20 @@ class TestPrefixer(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_infix_to_prefix(self):
-        self.assertEqual(infix_to_prefix('3'), '3')
-        self.assertEqual(infix_to_prefix('1 + 1'), '(+ 1 1)')
-        self.assertEqual(infix_to_prefix('2 * 5 + 1'), '(+ 1 (* 2 5))')
-        self.assertEqual(infix_to_prefix('2 * ( 5 + 1 )'), '(* (+ 1 5) 2)')
-        self.assertEqual(infix_to_prefix('3 * x + ( 9 + y ) / 4'), '(+ (* 3 x) (/ (+ 9 y) 4))')
+    # def test_infix_to_prefix(self):
+    #     self.assertEqual(infix_to_prefix('3'), '3')
+    #     self.assertEqual(infix_to_prefix('1 + 1'), '(+ 1 1)')
+    #     self.assertEqual(infix_to_prefix('2 * 5 + 1'), '(+ 1 (* 2 5))')
+    #     self.assertEqual(infix_to_prefix('2 * ( 5 + 1 )'), '(* (+ 1 5) 2)')
+    #     self.assertEqual(infix_to_prefix('3 * x + ( 9 + y ) / 4'), '(+ (* 3 x) (/ (+ 9 y) 4))')
+
+    # def test_infix_to_postfix(self):
+        # self.assertEqual(infix_to_postfix('3 + 4 * 2'), '(3 (4 2 *) +)')
+        # self.assertEqual(infix_to_postfix('3 * 4 - 2'), '((3 4 *) 2 -)')
+        # self.assertEqual(infix_to_postfix('3 * 1 + ( 9 + 1 ) / 4'), '((3 1 *) ((+ 9 1) 4 /) +)')
+
+    def test_queue_to_postfix(self):
+        self.assertEqual(queue_to_postfix(['3', '4', '*', '2', '-']), '((3 4 *) 2 -)')
 
 if __name__ == '__main__':
     unittest.main()
