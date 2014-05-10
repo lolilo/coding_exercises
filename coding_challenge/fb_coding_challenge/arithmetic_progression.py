e43346e5f7118c0902d0982b07fe8f9e1faab8c3
Hackbright Student <info@hackbrightacademy.com>
2014-05-10 15:09:40 -0700

    fixed and reorganized

--- coding_challenge/fb_coding_challenge/arithmetic_progression.py
+++ coding_challenge/fb_coding_challenge/arithmetic_progression.py
@@ -1,82 +1,55 @@
-
 # user_input = raw_input()
 
 n = "5"
-ap = "1 11 31 41 51"
+ap = "1 21 31 41 51"
 
 # arithmetic progression. 
 # two lines of raw input (stdin), first one being length of given progression, second the arithmetic progression
 # find the missing number and print it to stdout
 
-# ap = ap.split()
-# print ap
-
-def find_missing(n, ap):
-    ap = ap.split()
-    for i in range(len(ap)):
-        ap[i] = int(ap[i])
-        # print type(ap[0])
-
-    prev = ap[1] - ap[0]
-    curr = 0
-
-    for i in range(int(n)):
-        # print type(ap[i+1]), type(ap[i])
-        curr = ap[i + 1] - ap[i]
-        if curr != prev:
-            if ap[i+1] > ap[i]:
-                if prev < curr:
-                    return ap[i] + prev
-                else:
-                    return ap[i] + curr
-            else:
-                if prev > curr:
-                    return ap[i] + prev
-                else:
-                    return ap[i] + curr    
-
-print find_missing(n, ap)
-
-
-
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-n = int(raw_input())
-
 #user_input = user_input.split('\n')
-
 #print n
-
-ap = raw_input()
-# print ap
 
 def find_missing(n, ap):
 
     ap = ap.split()
+    # create a list of int
     for i in range(len(ap)):
         ap[i] = int(ap[i])
-        # print type(ap[0])
 
-    prev = ap[1] - ap[0]
-    curr = 0
+    init_diff = ap[1] - ap[0]
 
-    for i in range(int(n)):
-        # print type(ap[i+1]), type(ap[i])
-        curr = ap[i + 1] - ap[i]
-        if curr != prev:
-            if ap[i+1] > ap[i]:
-                if prev < curr:
-                    return ap[i] + prev
-                else:
-                    return ap[i] + curr
-            else:
-                if prev > curr:
-                    return ap[i] + prev
-                else:
-                    return ap[i] + curr
+    for i in range(int(n)-1):
+        this = ap[i]
+        next = ap[i+1]
+        curr_diff = next - this
 
+        if curr_diff != init_diff:
+            if next > this: # progression increasing
+                if init_diff < curr_diff: # insert @ curr_diff
+                    return this + init_diff
+                else: 
+                    return ap[0] + curr_diff
+            else: # progression decreasing
+                if init_diff < curr_diff: # abs(init_diff) > abs(curr_diff), insert @ init_diff
+                    return ap[0] + curr_diff
+                else: # missing number @ curr_diff
+                    return this + init_diff
 
-print find_missing(n, ap)
+def loop():
+    while True:
+        n = int(raw_input('n > '))
+        ap = raw_input('arithmetic progression > ')
+
+        print find_missing(n, ap)
+
+def main():
+    loop()
+
+if __name__ == "__main__":
+    main()
 
 
 # 45 minutes. Finished with 18 minutes to spare. 7 test cases. Didn't consider decreasing progression at first, so passed 5/7 tests. 
