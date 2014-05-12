11b4489c384eeb07f7ebc1521d016d082db9c466
Hackbright Student <info@hackbrightacademy.com>
2014-05-12 14:52:06 -0700

    find usernames in tweet

---
+++ string_manipulation/find_Twitter_usernames.py
@@ -0,0 +1,24 @@
+# Given a Tweet, find all usernames within the Tweet.
+import re
+USERNAME_PATTERN = re.compile('[.,:;?()\[\]>< ]@[\w]+')
+# \w indicates a word chracter
+# A word character is a character from a-z, A-Z, 0-9, 
+# including the _ (underscore) character.
+
+def find_usernames(tweet):
+    username_list = re.findall(USERNAME_PATTERN, tweet)
+
+    for i in range(len(username_list)):
+        username_list[i] = username_list[i][2:] # strip off delimiter and '@' from username
+        # check if username is valid. 
+        # if so, keep it in the list. 
+        # if not, delete it.
+
+    return username_list
+
+def main():
+    tweet = "Hello @Perforce! Hi?@Michael! This[@isatest. Does this )@pass?"
+    print find_usernames(tweet)
+
+if __name__ == "__main__":
+    main()
