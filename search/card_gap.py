f62631ed0a03d12993b7cc277c480967cc046fa2
LiLo <liana.lixia@gmail.com>
2014-09-15 13:17:34 -0700

    Update card_gap.py

--- search/card_gap.py
+++ search/card_gap.py
@@ -6,6 +6,7 @@ Find the card gap of an explosive.
 
 card_gap = 6
 
+# given function. returns True if n < card_gap (explodes), returns False otherwise (does not explode)
 def hammer(n):
 	if n < card_gap:
 		return True
@@ -15,15 +16,16 @@ def hammer(n):
 def card_gap(m):
 	start = 0 
 	end = m
-	while start <= end: 
-		middle = (end - start)//2 + start
-		if hammer(n): 
+	while start <= end: # might be able to end with just start < end, if we assume max possible is > 0. 
+		middle = (end - start)//2 + start # offset by start
+		if hammer(middle): 
 			start = middle + 1
 		else: 
 			end = middle - 1
-	return start
+	return start # make sure you understand why we return start and not end
 
 # edge case
-# start explodes, end does not. m = 1. Which means, start = 0, end = 1, middle = 0
-
+# consider a range of only length 2
+# start = middle = 0, end = 1
+# start explodes, end does not. m = 1//2 (+ start) = 0. 
 
