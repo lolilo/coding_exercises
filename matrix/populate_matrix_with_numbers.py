c769c06c5bebf6c9601795826ae4ee801a3208f5
LiLo <lilo@lilo-VirtualBox.(none)>
2014-09-16 10:04:39 -0700

    recursion update and file reorganization

---
+++ matrix/populate_matrix_with_numbers.py
@@ -0,0 +1,23 @@
+# VIRTUAL MEMORY/POINTERS. WHAT IS GOING ON. 
+
+m = [[0]*3]*3 # this will fuck shit up
+
+# the following two will produce independent rows
+m = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+m = [[0]*3, [0]*3, [0]*3]
+nums = range(1, 10)
+
+nums_pointer = 0
+i = 0 # m_pointer
+j = 0 # row_pointer
+
+while i < len(m):
+    row = m[i]
+    while j < len(row):
+        m[i][j] = nums[nums_pointer]
+        j += 1
+        nums_pointer += 1
+    j = 0
+    i += 1
+
+print m
--- recursion/complex_recursion/boggle.py
+++ recursion/complex_recursion/boggle.py
@@ -6,31 +6,54 @@ nrows, ncols = len(grid), len(grid[0])
 # A dictionary word that could be a solution must use only the grid's
 # letters and have length >= 3. (With a case-insensitive match.)
 import re
-alphabet = ''.join(set(''.join(grid)))
-# valid_boggle = re.compile('[' + alphabet + ']{3,}$', re.I).match # returns None if string does not match pattern
-# # valid_boggle is a function -- not great b/c we're doing work each time it's called
 
 
-pattern = '[' + alphabet + ']{3,}$'
-re_object = re.compile(pattern, re.I)
-def valid_boggle(re_object, word):
+# sets up words that may appear on board and their prefixes
+def set_up(DICTIONARY_PATH): # could also pass in different Boggle rule/valid pattern 
+	alphabet = ''.join(set(''.join(grid)))
+	# valid_boggle = re.compile('[' + alphabet + ']{3,}$', re.I).match # returns None if string does not match pattern
+	# valid_boggle is a function -- not great b/c we're doing work each time it's called
 
-	if re_object.match(word):
-		return True
-	else:
-		return False
+	pattern = '[' + alphabet + ']{3,}$'
+	re_object = re.compile(pattern, re.I)
+	def valid_boggle(re_object, word):
 
-input_file = open(DICTIONARY_PATH)
-set_of_words = set(input_file.read().splitlines()) # assume no blank lines in input file
+		if re_object.match(word):
+			return True
+		else:
+			return False
 
-valid_set_of_words = set()
-for word in set_of_words:
-	if valid_boggle(re_object, word):
-		valid_set_of_words.add(word)
+	input_file = open(DICTIONARY_PATH)
+	set_of_words = set(input_file.read().splitlines()) # assume no blank lines in input file
 
-print valid_set_of_words
+	# can also do list_of_words = input_file.readline().strip() with a for-loop 
+	# to individually filter out lines for blanks and other non-bogglable data
+	# this would also save us from constructing a separate set_of_words
+	valid_set_of_words = set()
+	for word in set_of_words:
+		if valid_boggle(re_object, word):
+			valid_set_of_words.add(word)
 
-# can also do list_of_words = input_file.readline().strip() with a for-loop 
-# to individually filter out lines for blanks and other invalid data
+	###### We now have a list of valid words that could possibly appear on the Boggle board.######
+	# print valid_set_of_words
 
+	# create a set of word prefixes
+	# could use a list comprehension here
+	prefixes = set()
+	for word in valid_set_of_words:
+		for i in range(2, len(word) + 1):
+			prefix = word[:i]
+			prefixes.add(prefix)
 
+	return valid_set_of_words, prefixes
+
+words, prefixes = set_up(DICTIONARY_PATH)
+# print words, '\n', prefixes
+
+def solve():
+	for y, row in enumerate(grid):
+		for x, letter in enumerate(row):
+			for result in extending(letter, ((x, y),)):
+				print result
+
+solve()
