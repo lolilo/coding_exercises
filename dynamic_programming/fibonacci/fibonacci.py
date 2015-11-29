7a80145adee597cbf0bac2848a51b568bc1ea3e2
Liana Lo <liana.lo@prezi.com>
2015-11-29 12:35:35 +0100

    [fib] unittest

--- dynamic_programming/fibonacci/fibonacci.py
+++ dynamic_programming/fibonacci/fibonacci.py
@@ -8,17 +8,34 @@ def fib_recursive(n):
 
 def fib_loop(n):
     a, b = 1, 1 # these are the first two
-    for i in range(n-2): # offset by the first two, n < 2 returns 1
+    for i in range(n-2): # offset by the first two, n < 2 returns 1 - don't like this. range(-1) = []. weird.
         temp = b
         b = a + b
         a = temp
     return b
+
+
+# we don't need to use space for the entire list
+def fib_list_save_space(n):
+    # if n < 2:
+    #     return 1
+    # don't need this check; taken care of in index <= n.
+    first, second = 1, 1
+    index = 3
+    while index <= n:
+        temp = second
+        second = first + second
+        first = temp
+        index += 1
+    return second
+
 
 def fib_loop_with_tuples(n):
     a, b = 1, 1
     for i in range(n-2):
         a, b = b, a+b
     return b
+
 
 def fib_memoization(n):
     def fib_loop(n):
@@ -60,13 +77,28 @@ def fib(n):
     # fancy stuff
     # return n if n < 2 else fib(n-2) + fib(n-1)
 
-def main():
+import unittest
 
+
+class Test(unittest.TestCase):
+
+    def test_fibonacci(self):
+        self.assertEqual(fib_recursive(10), 55)
+        self.assertEqual(fib_loop(10), 55)
+        self.assertEqual(fib_list_save_space(10), 55)
+        self.assertEqual(fib_loop_with_tuples(10), 55)
+        self.assertEqual(fib_memoization(10), 55)
+        self.assertEqual(fib(10), 55)
+
+
+def main():
     print fib_recursive(10)
     print fib_loop(10)
     print fib_loop_with_tuples(10)
     print fib_memoization(10)
     print fib(10)
 
+
 if __name__ == "__main__":
-    main()
+    # main()
+    unittest.main()
