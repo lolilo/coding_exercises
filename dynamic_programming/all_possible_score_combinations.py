96b8716b23e9e27950c5ccdb9cc6bd867dc6e8b5
Liana Lo <lilo@squareup.com>
2020-04-25 22:46:49 -0700

    does not work

--- dynamic_programming/all_possible_score_combinations.py
+++ dynamic_programming/all_possible_score_combinations.py
@@ -10,30 +10,43 @@ def memoize(fn):
     def decorated_fn(*args):
         # only need t as a key; assume scores array will be the same. 
         # breaks if method takes in varying scores.
-        key = args[1]
+        key = tuple((tuple(args[0]), args[1]))
 
         if not cache.get(key):
             cache[key] = fn(*args)
-        # print cache 
+        print cache 
         return cache[key]         
 
     return decorated_fn
 
 @memoize
 def add_to_total(scores, t): # takes in a list of ints and a target score
-    if t < min(scores):
-        return -1
-    if t == min(scores):
+    if t == 0:
         return 1
+    if t < min(scores):
+        return 0
+    # if t == min(scores):
+    #     return 1
 
     total_possible_ways = 0
-    # for score in scores:
     for score in [score for score in scores if score <= t]:
-        possible_ways = add_to_total(scores, t - score)
-        if possible_ways > 0:
-            total_possible_ways += possible_ways
+        if t == 3:
+            print 'score', score 
+            print 'total_possible_ways', total_possible_ways
+            print '' 
+        total_possible_ways += add_to_total(scores, t - score)
+        # if possible_ways > 0:   
+            # total_possible_ways += possible_ways
     return total_possible_ways
 
-l = [2, 6, 10]
-t = 10
-print add_to_total(l, t) == 3
+    # for score in [score for score in scores if score <= t]:
+    #     possible_ways = add_to_total(scores, t - score)
+    #     if possible_ways > 0:
+    #         total_possible_ways += possible_ways
+    # return total_possible_ways
+
+print add_to_total([2], 3) == 0 # [1, 1], [2]
+# print add_to_total([1, 2, 5], 2) == 2 # [1, 1], [2]
+print add_to_total([1, 2, 5], 3) == 2 # [1, 1, 1], [1, 2]
+# print add_to_total([1, 2, 5], 5) == 4
+# print add_to_total([2, 6, 10], 10) == 3
